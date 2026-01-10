36847ba490fbb7ff27c05d0ec2ed5b982d1ef985
0x1496FD0 <130846960+0x1496FD0@users.noreply.github.com>
2026-01-10 18:25:30 +0100

    v.3.00
    # Installation
      1. Drop the .prx inside __/data/GoldHEN/plugins__
         
      2. Register the .prx for your BO3 CUSA in __/data/GoldHEN/plugins.ini__
      
      3. Drop your __.gscc__ files inside __/data/T7 GSC Injector__ (the folder is auto created on run if non existent) and rename them accordingly to follow the convention gscc_{n} where __{n}__ is the index of the script (__ex: gscc_0   gscc_1   gscc_2  ...__)
    
    # Filezilla users
      Be carreful if you rename the .gscc file locally before transfering it, there is a bug on filezilla which cut 1 byte off from your file if it doesnt have an extension on transfer (experienced on arch linux filezilla 3.69.5-2 from the pacman repo but the same bug probably exist for windows and other versions)
    
    # Read this
        Make sure you are in the zm/mp/cp lobby before injecting
        You can add and remove .gscc while in game just make sure to respect the naming convention

---
+++ t7_gsc_injector_ps4/include/main.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Patcher.h>
+
+#include "../include/plugin_common.h"
+#include "../include/pad.h"
+#include "../include/fs.h"
+#include "../include/mem.h"
+#include "../include/game_offsets.h"
+
+#define VERSION "3.00"
+#define BUSY_INTERRUPT_S 1 // 1 second is ok, tested 500 it crashed the game every time
+
+char* g_notif_pfx = "⚡T7 GSC Injector";
+int g_errcode = 0;
+char g_notif_str[128] = {0};
+u8 g_busy = 0;
+u8 g_inputs = 1;
+char g_gsc_dir_str[128];
+char g_files_strarr[0xFF][256];
+u8 g_files_count = 0;
+char g_current_gsc_str[256] = {0};
+u8 g_current_gsc_index = 0;
+
+Patcher* g_pshellcode;
+s32 scePadReadStateExt(s32 handle, OrbisPadData* pad_data);
+s32 scePadReadState(s32 handle, OrbisPadData* pad_data);
+v inject();
+v switch_gsc(u8 next_or_prev);
+HOOK_INIT(scePadReadState);
