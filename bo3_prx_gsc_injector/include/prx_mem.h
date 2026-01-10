81ed941f021b052a0041beebe166a7f823cb1e30
0x1496FD0 <130846960+0x1496FD0@users.noreply.github.com>
2026-01-10 18:16:42 +0100

    updating to v.3.00
    -Code refactoring
    -Added a toggle to disable/re-enable inputs listenning

--- bo3_prx_gsc_injector/include/prx_mem.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "../include/plugin_common.h"
-
-
-void prx_mem_read(void* destination, u64 source, u32 nbytes);
-void prx_mem_write(void* source, u64 destination, u32 nbytes);
