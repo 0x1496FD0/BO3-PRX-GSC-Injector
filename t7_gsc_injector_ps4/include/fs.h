9289f3e69b98460cf7a31bed4bc8a3c61968dc5a
0x1496FD0 <130846960+0x1496FD0@users.noreply.github.com>
2026-01-27 01:35:40 +0100

    Delete t7_gsc_injector_ps4 directory

--- t7_gsc_injector_ps4/include/fs.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "types.h"
-
-s32 get_file_size(const char* path);
-s32 dir_or_file_exist(const char* path, s32 mode);
--- t7_gsc_injector_ps4/include/mem.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "../include/types.h"
-
-v mem_read(p destination, u64 source, u32 nbytes);
-v mem_write(p source, u64 destination, u32 nbytes);
